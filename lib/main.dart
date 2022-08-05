6022cbe8cdf9f83c394b65b3494fca3d6faa6f53
김민재 <kmj94102@naver.com>
2022-08-05 22:00:46 +0900

    입력폼 만들기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
 
 void main() {
   runApp(const MyApp());
@@ -7,21 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +30,255 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final formKey = GlobalKey<FormState>();
+  String? name = "";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SingleChildScrollView(
+          padding: EdgeInsets.only(top: 30, bottom: 30),
+          child: Form(
+            key: this.formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                /** 포켓몬 이름 **/
+                CustomTextField(
+                    context: context,
+                    hint: "포켓몬 이름",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 포켓몬 번호 **/
+                CustomTextField(
+                    context: context,
+                    hint: "포켓몬 번호",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 스테이터스 **/
+                Row(
+                  children: [
+                    Expanded(
+                        child: CustomTextField(
+                            context: context,
+                            inputType: TextInputType.number,
+                            hint: "HP",
+                            onSaved: (val) {
+                              setState(() {
+                                this.name = val;
+                              });
+                            }),
+                        flex: 1),
+                    Expanded(
+                        child: CustomTextField(
+                            context: context,
+                            hint: "공격",
+                            inputType: TextInputType.number,
+                            onSaved: (val) {
+                              setState(() {
+                                this.name = val;
+                              });
+                            }),
+                        flex: 1),
+                    Expanded(
+                        child: CustomTextField(
+                            context: context,
+                            inputType: TextInputType.number,
+                            hint: "방어",
+                            onSaved: (val) {
+                              setState(() {
+                                this.name = val;
+                              });
+                            }),
+                        flex: 1)
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: CustomTextField(
+                            context: context,
+                            inputType: TextInputType.number,
+                            hint: "특공",
+                            onSaved: (val) {
+                              setState(() {
+                                this.name = val;
+                              });
+                            }),
+                        flex: 1),
+                    Expanded(
+                        child: CustomTextField(
+                            context: context,
+                            inputType: TextInputType.number,
+                            hint: "특방",
+                            onSaved: (val) {
+                              setState(() {
+                                this.name = val;
+                              });
+                            }),
+                        flex: 1),
+                    Expanded(
+                        child: CustomTextField(
+                            context: context,
+                            inputType: TextInputType.number,
+                            hint: "스피드",
+                            onSaved: (val) {
+                              setState(() {
+                                this.name = val;
+                              });
+                            }),
+                        flex: 1)
+                  ],
+                ),
+                /** 설명 **/
+                CustomTextField(
+                    context: context,
+                    hint: "설명",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 분류 **/
+                CustomTextField(
+                    context: context,
+                    hint: "분류",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 특성 **/
+                CustomTextField(
+                    context: context,
+                    hint: "특성",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 속성 **/
+                CustomTextField(
+                    context: context,
+                    hint: "속성",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 도트이미지 **/
+                CustomTextField(
+                    context: context,
+                    hint: "도트이미지",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 도트 이로치 이미지 **/
+                CustomTextField(
+                    context: context,
+                    hint: "도트 이로치 이미지",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 이미지 **/
+                CustomTextField(
+                    context: context,
+                    hint: "이미지",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                /** 이로치 이미지 **/
+                CustomTextField(
+                    context: context,
+                    hint: "이로치 이미지",
+                    inputType: TextInputType.text,
+                    onSaved: (val) {
+                      setState(() {
+                        this.name = val;
+                      });
+                    }),
+                RaisedButton(
+                  onPressed: () async {
+                    if (this.formKey.currentState!.validate()) {
+                      this.formKey.currentState!.save();
+                      log("result $name");
+                    } else {
+                      log("fail $name");
+                    }
+                  },
+                  child: Text("저장하기"),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+void customShowDialog(
+  BuildContext context,
+  String title,
+) {
+  showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          actions: [
+            FlatButton(
+              child: Text('ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      });
+}
+
+Widget CustomTextField(
+    {required BuildContext context,
+    required FormFieldSetter onSaved,
+    required String hint,
+    required TextInputType inputType}) {
+  return Container(
+    padding: EdgeInsets.all(8),
+    child: Column(
+      children: [
+        TextFormField(
+            validator: (value) {
+              if (value!.length < 1) {
+                return value;
+              }
+            },
+            onSaved: onSaved,
+            keyboardType: inputType,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: hint,
+                errorStyle: TextStyle(color: Colors.red))),
+      ],
+    ),
+  );
 }
